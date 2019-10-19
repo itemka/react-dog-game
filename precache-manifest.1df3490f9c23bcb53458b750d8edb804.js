5c52776e0744dd5219d2bf5534c10fda9d3b05b9
itemka <itemka2503@gmail.com>
2019-10-19 21:31:50 +0300

    Updates

---
+++ precache-manifest.1df3490f9c23bcb53458b750d8edb804.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2506f6b03a6b45606bddcaf50886e7a1",
+    "url": "/ReactDogApp/index.html"
+  },
+  {
+    "revision": "f0178b549f98bd6afd03",
+    "url": "/ReactDogApp/static/css/main.7c2a3eb0.chunk.css"
+  },
+  {
+    "revision": "d0b449736ed267020629",
+    "url": "/ReactDogApp/static/js/2.908cb4aa.chunk.js"
+  },
+  {
+    "revision": "f0178b549f98bd6afd03",
+    "url": "/ReactDogApp/static/js/main.2e2dfa29.chunk.js"
+  },
+  {
+    "revision": "bf4d8176df76d588f54c",
+    "url": "/ReactDogApp/static/js/runtime-main.04278f3b.js"
+  },
+  {
+    "revision": "75fa68d4d95a95385dabee5a76f50797",
+    "url": "/ReactDogApp/static/media/dog.75fa68d4.mp3"
+  }
+]);
